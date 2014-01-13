03fa1de951304d903d219b4310da37859ce7d0ec
etissieres <etienne.tissieres@gmail.com>
2014-01-13 10:54:04 +0100

    name configurable sample

--- sample.js
+++ sample.js
@@ -1,5 +1,13 @@
+var commander = require('commander');
+
+commander
+  .version('0.0.1')
+  .option('-n, --name <n>', 'Container name')
+  .parse(process.argv);
+
 var hubiquitus = require('hubiquitus-core');
 hubiquitus.logger.enable('hubiquitus:addons:ping', 'trace');
-require('./index')('sample', 'http://localhost:1337');
+
+require('./index')(commander.name, 'http://localhost:1337');
 
 hubiquitus.start();
